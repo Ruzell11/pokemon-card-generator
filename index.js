d28812dc069d506af328ebad9814313ce375106a
Ruzell11 <johnruzell123@gmail.com>
2022-06-03 16:45:41 +0800

    deploy

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-const url = ' https://pokeapi.co/api/v2/pokemon/';
+const typeColor = {
+    bug: "#26de81",
+    dragon: "#ffeaa7",
+    electric: "#fed330",
+    fairy: "#FF0069",
+    fighting: "#30336b",
+    fire: "#f0932b",
+    flying: "#81ecec",
+    grass: "#00b894",
+    ground: "#EFB549",
+    ghost: "#a55eea",
+    ice: "#74b9ff",
+    normal: "#95afc0",
+    poison: "#6c5ce7",
+    psychic: "#a29bfe",
+    rock: "#2d3436",
+    water: "#0190FF",
+  }
+  
+  const url = ' https://pokeapi.co/api/v2/pokemon/';
 const card = document.getElementById('card');
 const button = document.getElementById('btn');
 
@@ -21,7 +40,12 @@ let getPokeData  = () => {
      const statAttack = data.stats[1].base_stat;
      const statDefense = data.stats[2].base_stat;
      const statSpeed = data.stats[5].base_stat;
-     
+       
+const themeColor = typeColor[data.types[0].type.name]
+  
+
+
+
      card.innerHTML =  `
      
      <p class="hp">
@@ -57,6 +81,7 @@ let getPokeData  = () => {
      
      `;
      appendTypes(data.types)
+     styleCard(themeColor )
     
    
  
@@ -70,6 +95,10 @@ let getPokeData  = () => {
         document.getElementById("types").appendChild(span)
     })
 };
+let styleCard = (color) => {
+    card.style.background = ` radial-gradient(circle at 50% 0% , 
+        ${color} 36% , #ffffff 36%)`
+    }
 
  button.addEventListener('click' , getPokeData);
 window.addEventListener("load" , getPokeData)
